2bcc3bd1daf2f0182fad9b7d72f7f64888c194a7
Anton Kulago <35004672+adeamann@users.noreply.github.com>
2018-01-04 16:45:42 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
- sudo rm -rf /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/mysql start
