31b202899c8f8895b80410ce5d4d04e8de280d64
adeamann <antej.deadmann@gmail.com>
2018-01-04 16:01:29 +0500

    First

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/mysql start
