cc8b1421589f3fb556ff820098cb14e8b47077d3
adeamann <antej.deadmann@gmail.com>
2018-01-06 16:26:14 +0500

    First

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo /etc/init.d/mysql start
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql restart
